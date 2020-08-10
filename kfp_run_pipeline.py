89314c84d80a89ed16624f4e68758cac96f251a2
muneer7589 <muneer7589@gmail.com>
2020-08-10 15:24:24 +0200

    included kubeflow host config and updated script for run accordingly

--- kfp_run_pipeline.py
+++ kfp_run_pipeline.py
@@ -1,10 +1,15 @@
 import kfp
+impost json
 
 experiment_id = '3a556a1f-6629-4b84-948c-79e4a6d95283'
 pipeline_id = '89b7a571-10d3-4ca7-9922-e8a847adfd12'
 run_name = 'testjenkinsrunpikfp'
+kubeflow_host = ''
+with open('./config/kubeflow.json') as kf_config:
+  data = json.load(kf_config)
+  kubeflow_host =  data['kubeflow'][0]['host']
 
 client = kfp.Client()
 
 run = client.run_pipeline(experiment_id = experiment_id, pipeline_id = pipeline_id, job_name = run_name)
-print('Run link: %s/#/runs/details/%s' % (client._get_url_prefix(), run.id))
+print('Run link: %s%s/#/runs/details/%s' % (kubeflow_host, client._get_url_prefix(), run.id))
