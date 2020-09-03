ca68fc770d7fcdf15a3314988f59209b9dd1ceec
Muneer Ahmad Dedmari <muneer7589@gmail.com>
2020-09-03 14:07:25 +0200

    Update kfpTest.py

--- kfpTest.py
+++ kfpTest.py
@@ -1,5 +1,9 @@
 import kfp
 
-client = kfp.Client()
+print(str(kfp.__version__))
+cookie_xsrf = "_xsrf=2|8ae5c4fb|72dfaf6152e6ec46122fb784c421578c|1596544754"
+cookie_authservice = "authservice_session=MTU5OTEyOTk0M3xOd3dBTkZnelVrWklURTlhVmxOWVFscE9WbEpMU1VNMVNrTk9OVkJGVnpSVFUxbFdWMHBJTlZwSE4wVlNRa1pDVVVKQ1ZVcEdUbEU9fILxlm8-tsJdO7tJJyuH8y6kAuKSPDDUJHLWIkytSAS1"
+combined_cookies = cookie_xsrf + "; " + cookie_authservice
+client = kfp.Client(namespace="admin", cookies=cookie_authservice)
 
-print(client.list_experiments())
+print(client.list_pipelines())
