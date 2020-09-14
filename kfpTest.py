2e9cc40ed3aaab7822a6fd5abe9c3d086f72d5e6
Muneer Ahmad Dedmari <muneer7589@gmail.com>
2020-09-14 12:55:56 +0200

    Update kfpTest.py

--- kfpTest.py
+++ kfpTest.py
@@ -5,5 +5,6 @@ print(str(kfp.__version__))
 #cookie_authservice = "authservice_session=MTU5OTEyOTk0M3xOd3dBTkZnelVrWklURTlhVmxOWVFscE9WbEpMU1VNMVNrTk9OVkJGVnpSVFUxbFdWMHBJTlZwSE4wVlNRa1pDVVVKQ1ZVcEdUbEU9fILxlm8-tsJdO7tJJyuH8y6kAuKSPDDUJHLWIkytSAS1"
 #combined_cookies = cookie_xsrf + "; " + cookie_authservice
 #client = kfp.Client(namespace="admin", cookies=cookie_authservice)
-client = kfp.Client()
+host = "https://172.30.92.10:443/pipeline"
+client = kfp.Client(host=host)
 print(client.list_pipelines())
